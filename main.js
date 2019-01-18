2d8f372da5e6d1cc88733335f2d11b7f9c757368
Primoz <sg.prima@gmail.com>
2019-01-18 09:48:42 +0100

    no moment

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ let win
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 400, height: 200 })
-  // win = new BrowserWindow({ width: 800, height: 600 })
+  // win = new BrowserWindow({ width: 400, height: 200 })
+  win = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
   win.loadFile('./pages/index.html')
 
   // Open the DevTools.
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
--- pages/app.js
+++ pages/app.js
@@ -1,5 +1,3 @@
-const moment = require('moment')
-
 const clockLabel = document.getElementById('clock')
 const startBtn = document.getElementById('startBtn')
 const pauseBtn = document.getElementById('pauseBtn')
@@ -12,27 +10,44 @@ const interval = 1000;
 let st = null;
 let running = false;
 
+function formatDate(date) {
+  let h = date.getHours()
+  let m = date.getMinutes()
+  let s = date.getSeconds()
+
+  return `${h}:${m}:${s}`
+}
+
+function formatUTCDate(date) {
+  let h = date.getUTCHours()
+  let m = date.getUTCMinutes()
+  let s = date.getUTCSeconds()
+
+  return `${h}:${m}:${s}`
+}
+
+function diffDate(a, b) {
+  let diff = a.getTime() - b.getTime()
+  return Math.abs(Math.round(diff))
+}
 
 setInterval(() => {
-  let ct = new Date().getTime()
-  clockLabel.innerHTML = moment(ct).format('HH:mm:ss')
+  let ct = new Date()
+  clockLabel.innerHTML = formatDate(ct)
 
   if (running && st != null) {
-    let diffTime = ct - st;
-    console.log(diffTime)
-
-    let duration = moment.duration(diffTime, 'milliseconds');
-    let x = `${duration.hours()}:${duration.minutes()}:${duration.seconds()}`
-    timer.innerHTML = x
+    let diff = new Date(diffDate(ct, st))
+    timer.innerText = formatUTCDate(diff)
   }
 }, interval);
 
 startBtn.addEventListener('click', (event) => {
   running = true
-  st = new Date().getTime()
-  startTime.innerHTML = moment(st).format('HH:mm:ss')
-  let et = st + (8 * 60 * 60 * 1000)
-  endTime.innerHTML = moment(et).format('HH:mm:ss')
+  st = new Date()
+  startTime.innerHTML = formatDate(st)
+
+  let et = new Date(st.getTime() + (8 * 60 * 60 * 1000))
+  endTime.innerHTML = formatDate(et)
 })
 
 pauseBtn.addEventListener('click', (event) => {
